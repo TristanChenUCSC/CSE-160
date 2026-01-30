e705417fdfb2775ce5af7957eeffd3d679bfe58d
Simpanzee <tristanzchen@gmail.com>
2026-01-30 14:38:57 -0800

    Assignment 2 Setup

---
+++ asgn2/src/Cube.js
@@ -0,0 +1,16 @@
+class Cube {
+    constructor() {
+        this.type = 'cube';
+        this.color = [1.0, 1.0, 1.0, 1.0];
+        //this.matrix = new Matrix4();
+    }
+
+    render() {
+        var rgba = this.color;
+
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);  
+
+        drawTriangle3D([0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0 ]);
+        drawTriangle3D([0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0 ]);
+    }
+}
---
+++ asgn2/src/asg2.js
@@ -0,0 +1,203 @@
+// ColoredPoint.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform float u_Size;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = u_Size;
+  }`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor;
+  }`
+
+// Global Variables
+let canvas;
+let gl;
+let a_Position;
+let u_FragColor;
+let u_Size;
+
+function setupWebGL() {
+  // Retrieve <canvas> element
+  canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+}
+
+function connectVariablesToGLSL() {
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // // Get the storage location of a_Position
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // Get the storage location of u_FragColor
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
+
+  // Get the storage location of u_Size
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
+}
+
+// Constants
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
+// UI Globals
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 5;
+let g_selectedType = POINT
+let g_circleSegments = 8;
+
+function addActionsForHtmlUI() {
+
+  // Buttons
+  document.getElementById('clearButton').onclick = function() { g_shapesList = []; undoStack = []; renderAllShapes(); };
+  document.getElementById('snakesButton').onclick = function() { drawSnakes(); };
+  document.getElementById('undoButton').onclick = function() { undo(); };
+  document.getElementById('redoButton').onclick = function() { redo(); };
+
+  document.getElementById('pointButton').onclick = function() { g_selectedType = POINT; };
+  document.getElementById('triButton').onclick = function() { g_selectedType = TRIANGLE; };
+  document.getElementById('circleButton').onclick = function() { g_selectedType = CIRCLE; };
+
+  // Sliders
+  document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100; });
+  document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100; });
+  document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100; });
+  document.getElementById('alphaSlide').addEventListener('mouseup', function() { g_selectedColor[3] = this.value/100; });
+
+
+  document.getElementById('sizeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
+  document.getElementById('segmentSlide').addEventListener('mouseup', function() { g_circleSegments = this.value; });
+}
+
+function main() {
+  setupWebGL();
+  connectVariablesToGLSL();
+
+  // Set up actions for HTML UI elements
+  addActionsForHtmlUI();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  renderAllShapes();
+}
+
+
+var g_shapesList = [];
+
+function click(ev) {
+  let [x, y] = convertCoordinatesEventToGL(ev);
+
+  // Create and store new point
+  let point;
+  if (g_selectedType == POINT) {
+    point = new Point();
+  }
+  else if (g_selectedType == TRIANGLE) {
+    point = new Triangle();
+  }
+  else if (g_selectedType == CIRCLE) {
+    point = new Circle();
+    point.segments = g_circleSegments;
+  }
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
+  undoStack = [];
+
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(ev) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  return [x, y];
+}
+
+function renderAllShapes() {
+
+  // Check the time at the start of this function
+  var startTime = performance.now();
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  drawTriangle3D([ -1.0,0.0,0.0, -0.5,-1.0,0.0, 0.0,0.0,0.0 ]);
+
+  var body = new Cube();
+  body.color = [1.0, 0.0, 0.0, 1.0];
+  body.render();
+  
+  // Check the time at the end of this function and show on webpage
+  var duration = performance.now() - startTime;
+  sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration)/10, "numdot");
+
+}
+
+// Set the text of a HTML element
+function sendTextToHTML(text, htmlID) {
+  var htmlElm = document.getElementById(htmlID);
+  if(!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
+}
+
+
+let undoStack = [];
+
+function undo() {
+  if (g_shapesList.length > 0) {
+    let shape = g_shapesList.pop();
+    undoStack.push(shape);
+    renderAllShapes();
+  }
+}
+
+function redo() {
+  if (undoStack.length > 0) {
+    let shape = undoStack.pop();
+    g_shapesList.push(shape);
+    renderAllShapes();
+  }
+}
