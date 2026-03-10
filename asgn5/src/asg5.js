baaafdfc2834c31fb51625f832c91f5915b3fbb7
Simpanzee <tristanzchen@gmail.com>
2026-03-10 15:36:11 -0700

    Added skybox, gojo, shrine, and music

--- asgn5/src/asg5.js
+++ asgn5/src/asg5.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { MinMaxGUIHelper } from './MinMaxGUIHelper.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 function main() {
+    const music = document.getElementById('bg-music');
+    music.volume = 0.2;
+    music.play();
+
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
@@ -10,7 +18,19 @@ function main() {
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    camera.position.x = 0; //8
+    camera.position.y = 6; //6
+    camera.position.z = 5; //8
+
+    const gui = new GUI();
+    gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
+    gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
+    gui.add(minMaxGUIHelper, 'max', 0.1, 100, 0.1).name('far').onChange(updateCamera);
+
+    const controls = new OrbitControls( camera, canvas );
+	controls.target.set( 0, 5, 0 );
+	controls.update();
 
     // Scene
     const scene = new THREE.Scene();
@@ -20,25 +40,236 @@ function main() {
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
 
-    // Mesh
-    const cube = new THREE.Mesh(geometry, material);
+    // Skybox
+    {
+        const skyboxLoader = new THREE.CubeTextureLoader();
+        const texture = skyboxLoader.load([
+          '../assets/void1.jpg',
+          '../assets/void3.jpg',
+          '../assets/void5.jpg',
+          '../assets/void6.jpg',
+          '../assets/void2.jpg',
+          '../assets/void4.jpg',
+        ]);
+        scene.background = texture;
+    }
+    
 
-    scene.add(cube);
+    // Meshes
+    const gojo = createGojo();
+    scene.add(gojo);
+
+    // Lighting
+    const color = 0xFFFFFF;
+    const intensity = 3;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 0, 0); //(-1, 2, 4)
+    scene.add(light);
+
+    const light2 = new THREE.DirectionalLight(color, intensity);
+    light2.position.set(1, 0, 0);
+    scene.add(light2);
+
+    const light3 = new THREE.DirectionalLight(color, intensity);
+    light3.position.set(0, 0, -1);
+    scene.add(light3);
+
+    const light4 = new THREE.DirectionalLight(color, intensity);
+    light4.position.set(0, 0, 1);
+    scene.add(light4);
+
+    // Objects
+    // const gltfLoader = new GLTFLoader;
+    // gltfLoader.load(
+    //     '../assets/Malevolent_shrine.glb',
+
+    //     function (gltf) {
+    //         const model = gltf.scene;
+    //         model.position.z = -6
+    //         scene.add(model);
+    //     }
+    // )
+
+
     renderer.render(scene, camera);
+
+    function updateCamera() {
+        camera.updateProjectionMatrix();
+    }
 
     function render(time) {
         time *= 0.001;  // convert time to seconds
-       
-        cube.rotation.x = time;
-        cube.rotation.y = time;
        
         renderer.render(scene, camera);
        
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
+
+
+    function makeInstance(geometry, color, x) {
+        const material = new THREE.MeshPhongMaterial({color});
+       
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+       
+        cube.position.x = x;
+       
+        return cube;
+    }
+}
+
+function createGojo() {
+    const group = new THREE.Group();
+
+    const skin = new THREE.MeshPhongMaterial({ color: 0xffdbac });
+    const black = new THREE.MeshPhongMaterial({ color: 0x111111 });
+    const white = new THREE.MeshPhongMaterial({ color: 0xf2f2f2 });
+    const blue = new THREE.MeshPhongMaterial({ color: 0x2ec0ff });
+
+    // hair
+    const gltfLoader = new GLTFLoader;
+    gltfLoader.load(
+        '../assets/hair.glb',
+
+        function (gltf) {
+            const model = gltf.scene;
+            model.scale.set(2,2,2);
+            model.position.y = 1.3;
+            group.add(model);
+        }
+    )
+
+    // head
+    const head = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5,32,16),
+        skin
+    );
+    head.position.y = 6;
+    group.add(head);
+
+    // Eyes
+    const leftEye = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.08,0.08,0.5,16),
+        blue
+    );
+    leftEye.rotation.x = Math.PI / 2;
+    leftEye.position.x = -0.15;
+    leftEye.position.y = 6;
+    leftEye.position.z = 0.25;
+    group.add(leftEye);
+
+    const rightEye = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.08,0.08,0.5,16),
+        blue
+    );
+    rightEye.rotation.x = Math.PI / 2;
+    rightEye.position.x = 0.15;
+    rightEye.position.y = 6;
+    rightEye.position.z = 0.25;
+    group.add(rightEye);
+
+    // Body
+    const body = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        black
+    );
+    body.scale.set(0.8, 1.2, 0.8);
+    body.position.set(0, 4.95, 0);    
+    group.add(body);
+
+    // legs
+    const leftUpperLeg = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        white
+    );
+    leftUpperLeg.scale.set(0.33, 0.9, 0.8 );
+    leftUpperLeg.rotation.set(-Math.PI / 3, 0, 0);
+    leftUpperLeg.position.set(-0.23, 4.2, 0.1);    
+    group.add(leftUpperLeg);
+
+    const leftLowerLeg = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        white
+    );
+    leftLowerLeg.scale.set(0.33, 0.9, 0.6 );
+    leftLowerLeg.position.set(-0.23, 3.86, 0.39);    
+    group.add(leftLowerLeg);
+
+    const rightLeg = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        white
+    );
+    rightLeg.scale.set(0.33, 1.2, 0.79 );
+    rightLeg.position.set(0.23  , 4, 0);    
+
+    group.add(rightLeg);
+
+    // arms
+    const leftShoulder = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.3,16),
+        black
+    );
+    leftShoulder.rotation.set(Math.PI / 2, 0, 0);
+    leftShoulder.position.set(-0.55, 5.2, 0);    
+    group.add(leftShoulder);
+
+    const leftUpperArm = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.4,16),
+        skin
+    );
+    leftUpperArm.rotation.set(Math.PI / 2, 0, 0);
+    leftUpperArm.position.set(-0.55, 5.2, 0.35);    
+    group.add(leftUpperArm);
+
+    const leftLowerArm = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.5,16),
+        skin
+    );
+    leftLowerArm.rotation.set(degToRad(60), 0, 0);
+    leftLowerArm.position.set(-0.55, 5.3, 0.7);    
+    group.add(leftLowerArm);
+
+    const rightShoulder = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.3,16),
+        black
+    );
+    rightShoulder.rotation.set(degToRad(45), 0, 0);
+    rightShoulder.position.set(0.55, 5.2, 0);    
+    group.add(rightShoulder);
+
+    const rightUpperArm = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.27,16),
+        skin
+    );
+    rightUpperArm.rotation.set(degToRad(45), 0, 0);
+    rightUpperArm.position.set(0.55, 5, -0.2);    
+    group.add(rightUpperArm);
+
+    const rightLowerArm = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.18,0.18,0.5,16),
+        skin
+    );
+    rightLowerArm.position.set(0.55, 4.75, -0.24);    
+    group.add(rightLowerArm);
+
+    return group;
+}
+
+function createSukuna(){
+    const group = new THREE.Group();
+
+    const skin = new THREE.MeshPhongMaterial({ color: 0xffdbac });
+    const black = new THREE.MeshPhongMaterial({ color: 0x111111 });
+    const white = new THREE.MeshPhongMaterial({ color: 0xf2f2f2 });
+    const red = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+
+    
+}
+
+function degToRad(deg) {
+    return deg * Math.PI / 180;
 }
 
 main();
